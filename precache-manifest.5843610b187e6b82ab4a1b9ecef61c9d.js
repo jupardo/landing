4a100c0427a31c4096a60e130a63b79e89a398ba
jupardo <jd.pardo@uniandes.edu.co>
2020-08-27 23:16:57 -0500

    Updates

---
+++ precache-manifest.5843610b187e6b82ab4a1b9ecef61c9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d5d9a4dae56768f603857273b4389b",
+    "url": "/landing/index.html"
+  },
+  {
+    "revision": "34e4fd1197f7759ddb57",
+    "url": "/landing/static/css/main.dede850d.chunk.css"
+  },
+  {
+    "revision": "c68538b1ac3a8e333393",
+    "url": "/landing/static/js/2.bd5cd54a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landing/static/js/2.bd5cd54a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e4fd1197f7759ddb57",
+    "url": "/landing/static/js/main.6eb16ea2.chunk.js"
+  },
+  {
+    "revision": "2b02675cd41264016341",
+    "url": "/landing/static/js/runtime-main.9bc6adf9.js"
+  },
+  {
+    "revision": "911a76a00231b0eab3385d3c13d2f6e4",
+    "url": "/landing/static/media/audifonosmascara2.911a76a0.jpg"
+  },
+  {
+    "revision": "df8bc0cbce64bf2eb58e7144cbc55e11",
+    "url": "/landing/static/media/texture-2068283_1920.df8bc0cb.jpg"
+  }
+]);
